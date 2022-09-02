04aa743755be7ea901f89b8d282cfc2bdd5a448d
yu-ko-ba <yu-ko-ba@outlook.com>
2022-09-02 15:59:06 +0900

    フォントを変えた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
-import { TextField, Typography } from '@mui/material'
+import { Box, Container, TextField, ThemeProvider, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { useState } from 'react'
+import theme from '../theme/theme'
 import insertDakuten from '../utils/insertDakuten'
 
 const Home: NextPage = () => {
   const [outText, setOutText] = useState('')
   return (
-    <>
-      <TextField
-        onChange={(e) => {
-          setOutText(insertDakuten(e.target.value))
-        }}
-      >
-      </TextField>
-      <Typography>{outText}</Typography>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+        <TextField
+          onChange={(e) => {
+            setOutText(insertDakuten(e.target.value))
+          }}
+        >
+        </TextField>
+        <Typography>{outText}</Typography>
+      </Container>
+    </ThemeProvider>
   )
 }
 
