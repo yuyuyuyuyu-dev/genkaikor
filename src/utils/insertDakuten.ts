44983f3c1644cda204318fd1c8b4243128267f58
yu-ko-ba <yu-ko-ba@outlook.com>
2022-09-24 19:55:06 +0900

    ーと〜と改行には濁点を付けないようにした

--- src/utils/insertDakuten.ts
+++ src/utils/insertDakuten.ts
@@ -1,5 +1,5 @@
 const insertDakuten = (str: string): string => { 
-  const ignores = ['!', '?', '！', '？']
+  const ignores = ['ー', '〜', '!', '?', '！', '？', '\n']
   let result = ""
   for (const s of str) {
     if (ignores.includes(s)) {
